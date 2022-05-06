6b2fb2a56e7298fec30c7e0b864b6836704a2cbb
Роман Шапарь <romanshaparution@gmail.com>
2022-05-06 21:25:19 +0300

    added

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -8,42 +8,38 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
-
     @BeforeEach
     public void openPage() {
         open("http://localhost:9999/");
     }
 
+    // Положительные
+
+    @Test
+    public void shouldReturnValid() {
+        $("[data-test-id=name] input").setValue("Артем Петров");
+        $("[data-test-id=phone] input").setValue("+79862146556");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    public void shouldReturnValidValueOneWord() {
+        $("[data-test-id=name] input").setValue("Игнат");
+        $("[data-test-id=phone] input").setValue("+45475675841");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
     @Test
     public void shouldReturnValidValue() {
-        $(".form-field_theme_alfa-on-white");
         $("[data-test-id=name] input").setValue("Степан Степанов");
         $("[data-test-id=phone] input").setValue("+77895478932");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
-    @Test
-    public void shouldShowErrorInvalidNumberFieldEmpty() {
-        $(".form-field_theme_alfa-on-white");
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("");
-        $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $(".input_type_text .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    public void shouldShowErrorInvalidName() {
-        $(".form-field_theme_alfa-on-white");
-        $("[data-test-id=name] input").setValue("Ivanov Ivan");
-        $("[data-test-id=phone] input").setValue("+1236547895");
-        $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $(".input_type_text .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-
-
 
 }
---
+++ src/test/java/ru/netology/CardOrderTestFailed.java
@@ -0,0 +1,64 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTestFailed {
+
+    @BeforeEach
+    public void openPage() {
+        open("http://localhost:9999/");
+    }
+    //Отрицательные тесты
+    //
+    @Test
+    public void shouldShowErrorInvalidNumberFieldEmpty() {
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldShowErrorInvalidName() {
+        $("[data-test-id=name] input").setValue("Ivanov Ivan");
+        $("[data-test-id=phone] input").setValue("+1236547895");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+
+
+    @Test
+    public void shouldShowErrorNoSymbol() {
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(Condition.exactText("Поля обязательные для заполнения"));
+    }
+
+
+    @Test
+    public void shouldShowErrorInvalidNumberTelephoneField() {
+        $("[data-test-id=name] input").setValue("Андрей Андреев");
+        $("[data-test-id='phone'].inputinvalid .input__sub").setValue("+77647578874764764");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_tel .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void shouldShowErrorInvalidAgreement() {
+        $("[data-test-id=name] input").setValue("Владимир Вованов");
+        $("[data-test-id=phone] input").setValue("+7986687656");
+        $("[type=button]").click();
+        $(".input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+}
