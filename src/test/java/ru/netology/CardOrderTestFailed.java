0ef9baa7a1db1287fcf936dd29b9127c43244053
Роман Шапарь <romanshaparution@gmail.com>
2022-05-12 14:37:57 +0300

    modif

--- src/test/java/ru/netology/CardOrderTestFailed.java
+++ src/test/java/ru/netology/CardOrderTestFailed.java
@@ -33,7 +33,14 @@ public class CardOrderTestFailed {
         $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
-
+    @Test
+    public void shouldShowErrorInvalidNumberTelephoneField() {
+        $("[data-test-id=name] input").setValue("Игорь Игорев");
+        $("[data-test-id=phone] input").setValue("+1549646236547895");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 
     @Test
     public void shouldShowErrorNoSymbol() {
@@ -41,24 +48,6 @@ public class CardOrderTestFailed {
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $("[data-test-id=''].input_invalid .input__sub").shouldHave(Condition.exactText("Поля обязательные для заполнения"));
-    }
-
-
-    @Test
-    public void shouldShowErrorInvalidNumberTelephoneField() {
-        $("[data-test-id=name] input").setValue("Игорь Игорев");
-        $("[data-test-id=phone] input").setValue("+1549646236547895");
-        $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
-    public void shouldShowErrorInvalidAgreement() {
-        $("[data-test-id=name] input").setValue("Владимир Вованов");
-        $("[data-test-id=phone] input").setValue("+7986687656");
-        $("[type=button]").click();
-        $(".input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $(".input_type_text .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 }
